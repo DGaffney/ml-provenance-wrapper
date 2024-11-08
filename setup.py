1a54193bfa6ec1dbdd0864017d10af88849576f2
Devin Gaffney <itsme@devingaffney.com>
2024-11-08 05:31:53 -0800

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='model_provenance',
-    version='0.1.0',
+    version='0.1.1',
     description='A package to track model metadata and provenance for scikit-learn and other ML models',
     packages=find_packages(),
     install_requires=[
